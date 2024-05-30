d1d46d54de6d7b3610456281356ca65ef9b2d7de
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-05-30 07:58:38 -0300

    atualizado

--- curso-video/listas/valores.py
+++ curso-video/listas/valores.py
@@ -1,8 +1,18 @@
-valores = []
-valores.append(5)
-valores.append(9)
-valores.append(4)
+# TODO: Crie uma Lista 'itens' para armazenar os equipamentos:
+itens = []
+#itens = ["Antena", "Roteador", "Switch", "Servidor", "Cabine Pack", "Access Popint", "Edge Routers", "Patch cord", "Ups"]
 
-for v in valores:
-    #print(f'{v}...')# valor em baixo de valor
-    print(f'{v}...', end='') # um do lado do outro 
+# TODO: Crie um loop para solicita os itens ao usuário:
+
+# TODO: Solicite o item e armazena na variável "item":
+
+# TODO: Adicione o item à lista "itens":
+itens.append('antena')
+itens.append('roteador')
+itens.append('switch')
+
+# Exibe a lista de itens
+print("Lista de Equipamentos:")  
+for item in itens:
+    # Loop que percorre cada item na lista "itens"
+    print(f"- {item}")
