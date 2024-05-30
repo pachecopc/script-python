a6d14a63dbad28ee9b2be6907f0b16f1a45dba05
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-05-30 09:14:58 -0300

    mundo1-aula1a5

---
+++ curso-video/mundo1/aula_1_a_5_conhecendo a linguagem/desafio_1.py
@@ -0,0 +1,4 @@
+#crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado, e a 1º letra sera  maiuscula.
+
+entrada = input('Digite seu nome: \n')
+print('Olá',entrada.title(),'prazer te conhecer!'.format(entrada))
---
+++ curso-video/mundo1/aula_1_a_5_conhecendo a linguagem/desafio_3.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+num1 = int(input('Primeiro número: \n'))
+num2 = int(input('Segundo número: \n'))
+print('A soma é:', num1 + num2)
+print('Á soma é: %d' % (num1 + num2))
+print('Á soma é: {}'.format(num1 + num2))
