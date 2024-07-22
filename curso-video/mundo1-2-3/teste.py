e8dd24a9c7b05ae4ce5d2a6bbc3065468dc6f309
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-07-22 07:42:41 -0300

    atualizado

--- curso-video/mundo1-2-3/teste.py
+++ curso-video/mundo1-2-3/teste.py
@@ -1,7 +1,10 @@
-# TESTES
+''' TESTES
 for p in range(1, 5):
     print('------- {}ª Pessoa -------'.format(p))
     nome = input('Meu o nome: ')
     print('Prazer em te conhecer ')   
-print('FIM')  
+print('FIM')  '''
+
+
+
 
