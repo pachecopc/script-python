2f2e51334035777399a4c551b16d868d2c083dfd
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-07-24 12:00:09 -0300

    atualizado

--- curso-video/mundo1-2-3/m1desafio_014.py
+++ curso-video/mundo1-2-3/m1desafio_014.py
@@ -1,3 +1,5 @@
+'''Exercício Python 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+'''
 c = float(input('Informe a temperatura em C: '))
 f = 9 * c / 5 + 32
 print('A temperatura de {} C corresponde a {} F' .format(c, f) )
