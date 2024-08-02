70e8f6ad6b6424ecf72a14f31e5481841a9713e1
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-08-02 06:02:20 -0300

    atualize

---
+++ curso-video/mundo1-2-3/m1desafio_016.py
@@ -0,0 +1,6 @@
+'''Exercício Python 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+'''
+num = float(input('Digite um número: '))
+print('O valor digitado foi {}, e sua porção inteira é {}'.format(num,int(num)))
+
+print('======================= Fim do Programa ====================')
