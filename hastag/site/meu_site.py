67544ab4922c6c9a56c4dda5af62c986b68f4d95
Paulo <120028058+pachecopc@users.noreply.github.com>
2025-01-13 11:00:28 -0300

    deploy inicial

---
+++ hastag/site/meu_site.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#criar a 1º pagina do site
+#route -> hastegtreinamentos.com (é a rota que vai ser siguida)
+#função ->o que você quer exibir naquela pagina
+@app.route("/")
+def homepage():
+    return  render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return  render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return  render_template("usuarios.html", nome_usuario=nome_usuario)
+
+#colocar o site no ar 
+if __name__ == "__main__":
+
+ app.run(debug =True)
+
+ requirements.txt
