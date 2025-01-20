7587975dda23d591bc8e673da28458265fdc99a2
Paulo <paulopacheco7@hotmail.com>
2025-01-20 11:22:00 -0300

    atualizado

--- alura/jogoteca/jogoteca.py
+++ alura/jogoteca/jogoteca.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template, request, redirect
 
 class Jogo:
     def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
-
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
 jogo1 = Jogo('Tretis', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of war', 'Rack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Luta','PS2') 
 lista = [jogo1, jogo2, jogo3]       
 
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def lista():
-    return render_template('lista.html', titulo ='Jogos', jogos = lista )
+def index():
+    
+    return render_template('lista.html', titulo='Jogos', jogos=lista )
 
 @app.route('/novo')
 def novo():
@@ -28,5 +30,6 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect ('/')
+    return redirect('/')
+
 app.run(debug=True)    
