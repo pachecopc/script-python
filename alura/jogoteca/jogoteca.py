0564907a2e2e8fc4c688373e7a4b8ab8e68e5cb9
Paulo <paulopacheco7@hotmail.com>
2025-01-25 02:12:32 -0300

    atualizado

--- alura/jogoteca/jogoteca.py
+++ alura/jogoteca/jogoteca.py
@@ -1,38 +1,50 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome=nome
-        self.categoria=categoria
-        self.console=console
-jogo1 = Jogo('Tretis', 'Puzzle', 'Atari')
-jogo2 = Jogo('God of war', 'Rack n Slash', 'PS2')
-jogo3 = Jogo('Mortal Kombat', 'Luta','PS2') 
-lista = [jogo1, jogo2, jogo3]       
-
-
+     def __init__(self, nome, categoria, console):
+         self.nome = nome
+         self.categoria = categoria
+         self.console = console
+jogo1 = Jogo('tretis', 'puzzle', 'atari')
+jogo2 = Jogo('god of war', 'rack n slash', 'ps2')
+jogo3 = Jogo('mortal kombate', 'luta', 'ps2')     
+lista = [jogo1, jogo2, jogo3]          
 
 app = Flask(__name__)
-#app.secret_key = 'mundo'
+app.secret_key = 'mundo'
 
-@app.route('/')
-def index():    
-    return render_template('lista.html', titulo='Jogos', jogos=lista )
 
-@app.route('/novo')
+@app.route("/")
+def index():     
+     return render_template('lista.html', titulo = 'Jogos', jogos=lista)
+
+
+@app.route("/novo")
 def novo():
-    return render_template('novo.html', titulo='Novo Jogo')
+     return render_template('novo.html', titulo='Novo Jogo')
 
-@app.route('/criar', methods=['POST',])
+@app.route('/criar', methods=['POST' ,])
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
-    return redirect('/')
+     nome = request.form['nome']
+     categoria = request.form['categoria']
+     console = request.form['console']
+     jogo = Jogo(nome, categoria, console)
+     lista.append(jogo)
+     return redirect('/')
+
+@app.route('/login')
+def login():
+     return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST' ,])
+def autenticar():
+     if 'pacheco' == request.form['senha']:
+          session['usuario_logado'] = request.form['usuario']
+          flash(session['usuario_logado']  +  ' logado com sucesso!')
+          return redirect('/')
+     else:
+          flash('Usuário nao Logado')
+          return redirect('/login') 
 
 
-
-
-app.run(debug=True)    
+app.run(debug=True)
